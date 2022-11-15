72f1e937e7e82ec9f81ef7a318fb8b70db1a1289
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2022-11-16 01:09:13 +0600

    Initial commit

---
+++ src/api/starter/controllers/starter.ts
@@ -0,0 +1,7 @@
+/**
+ * starter controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::starter.starter');
---
+++ src/api/starter/services/starter.ts
@@ -0,0 +1,7 @@
+/**
+ * starter service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::starter.starter');
